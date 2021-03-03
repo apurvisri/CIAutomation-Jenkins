ede342b2bfb50b3955729f273e07dadaacd1e9d3
apurvisri <41233996+apurvisri@users.noreply.github.com>
2021-03-03 22:58:09 +0530

    Update test_flask_app.py

--- cd-pipeline/tests/test_flask_app.py
+++ cd-pipeline/tests/test_flask_app.py
@@ -3,19 +3,23 @@
 import os
 import sys
 import unittest
+# Flask application we wrote
 import app
 
+#This uses the unittest framework
+#setUp and tearDown are common methods in testing frameworks, used to initialised the mock objs 
+#or tasks and clean up the env after a test is executed
 class BasicTest(unittest.TestCase):
-
-    #execute before each test
+    
+    #execute before each test to mock a flask app
     def setUp(self):
         self.app = app.app.test_client()
 
-    #execute after each test
+    #execute after each test 
     def tearDown(self):
         pass
 
-    #test
+    #test, this methods gets the mock falsk app and checks that the root of the app returns 200 http status code  
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=Tue)
         self.assertEqual(response.status_code, 200)
